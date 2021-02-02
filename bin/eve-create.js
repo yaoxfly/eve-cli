440888165cc12683839dfac465b309cdfd0bfad2
yaoxfly <1315892088@qq.com>
2021-02-02 17:45:44 +0800

    feat:添加更多的交互

--- bin/eve-create.js
+++ bin/eve-create.js
@@ -1,49 +1,70 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const program = require('commander')
 const chalk = require('chalk')
 const ora = require('ora')
 const download = require('download-git-repo')
 const tplObj = require(`${__dirname}/../template`)
-
+const symbols = require('log-symbols');
+const handlebars = require('handlebars')
+const inquirer = require('inquirer');
 program
-    .usage('<template-name> [project-name]')
+    .usage('<project-name>  or <template-name>')
 program.parse(process.argv)
 // 当没有输入参数的时候给个提示
 if (program.args.length < 1) return program.help()
-
-// 好比 vue init webpack project-name 的命令一样，第一个参数是 webpack，第二个参数是 project-name
 let templateName = program.args[0]
-let projectName = program.args[1]
-// 小小校验一下参数
-if (!tplObj[templateName]) {
-    console.log(chalk.red('\n Template does not exit! \n '))
-    return
-}
-if (!projectName) {
-    console.log(chalk.red('\n Project should not be empty! \n '))
-    return
-}
+//有模板就用自定义的模板,没有就用默认的模板
+let url = tplObj[templateName] ? tplObj[templateName] : tplObj['eve-admin']
 
-url = tplObj[templateName]
-
-console.log(chalk.white('\n Start generating... \n'))
-// 出现加载图标
-const spinner = ora("Downloading...");
-spinner.start();
-// 执行下载方法并传入参数
-download(
-    url,
-    projectName,
-    err => {
-        if (err) {
-            spinner.fail();
-            console.log(chalk.red(`Generation failed. ${err}`))
-            return
-        }
-        // 结束加载图标
-        spinner.succeed();
-        console.log(chalk.green('\n Generation completed!'))
-        console.log('\n To get started')
-        console.log(`\n    cd ${projectName} \n`)
+const param = [
+    {
+        type: 'input',
+        name: 'description',
+        message: '请输入项目描述'
     }
-)
+]
+tplObj[templateName] && param.unshift({
+    type: 'input',
+    name: 'name',
+    message: '请输入项目名称'
+})
+
+inquirer.prompt(param).then((answers) => {
+    let { name = templateName, description } = answers || {}
+    if (fs.existsSync(name)) {
+        console.log(chalk.red("项目已存在"))
+        return
+    }
+    console.log(chalk.white('\n Start generating... \n'))
+    // 出现加载图标
+    const spinner = ora("Downloading...");
+    spinner.start();
+    // 执行下载方法并传入参数
+    download(
+        url,
+        name,
+        err => {
+            if (err) {
+                spinner.fail();
+                console.log(chalk.red(`Generation failed. ${err}`))
+                return
+            }
+            const meta = {
+                description: description,
+                name: name
+            }
+            const fileName = `${name}/package.json`;
+            const content = fs.readFileSync(fileName).toString();
+            const result = handlebars.compile(content)(meta);
+            fs.writeFileSync(fileName, result);
+            // 结束加载图标
+            spinner.succeed();
+            console.log(symbols.success, chalk.green('项目初始化完成'));
+            console.log('\n To get started')
+            console.log(`\n cd ${name}`)
+            console.log(`\n npm run serve \n`)
+        }
+    )
+})
+
